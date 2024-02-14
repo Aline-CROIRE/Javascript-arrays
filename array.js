d2340e1526063a0ba87b246b4207cca915dee2d4
Aline NIYONIZERA <nikyocroirealine@gmail.com>
2024-02-13 23:51:41 -0800

    array

---
+++ array.js
@@ -0,0 +1,24 @@
+let numbers=[-4,5,6];
+let colors=['blue','red','yellow'];
+/*let doublenum=numbers.map(num=>num*2);
+console.log(doublenum);
+let oddnumbers= numbers.filter(num=>num%2!== 0);
+console.log(oddnumbers);
+ let allnumbers= numbers.reduce((acc,num)=> acc+num,30);
+ console.log(allnumbers);
+ let allPositive = numbers.every(num => num > 0);
+ console.log(allPositive);
+ let elementreverse=numbers.reverse();
+ console.log(elementreverse);
+ let indexof4=numbers.indexOf(-4);
+ console.log(indexof4);
+ let sortNumbers= numbers.sort((a,b)=>a-b);
+ console.log(sortNumbers);*/
+ console.log(numbers.reverse());
+ console.log(numbers.slice(0,1));
+console.log(numbers.indexOf(5));
+let arraycombined=numbers.concat(colors);
+console.log(arraycombined);
+
+
+    
